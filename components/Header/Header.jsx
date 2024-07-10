f5caded64b621e0a988a04f7b2ebb3c22406b968
fridalif <124243435+fridalif@users.noreply.github.com>
2024-07-10 21:34:34 +0300

    created routing between home and login

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -10,21 +10,22 @@ export class Header extends Component {
                 <div className={styles.logoBlock}>
                    <Image src={logoSrc} alt='Логотип' width={100} height={140}/>
                 </div>
-                <div className={styles.titleBlock}>
-                   <b> IT Глоссарий </b>
-                </div>
+                <a href='/'>
+                    <div className={styles.titleBlock} onclick={() => window.location.href = '/'}>
+                        <b> IT Глоссарий </b>
+                    </div>
+                </a>
                 <div className={styles.documentsHeaderBlock}>
                     Документы
                 </div>
                 <div className={styles.terminsHeaderBlock}>
                     Термины
                 </div>
-                <div className={styles.registerHeaderBlock}>
-                    Регистрация
-                </div>
-                <div className={styles.enterHeaderBlock}>
-                    Вход
-                </div>
+                <a href='/login'>
+                    <div className={styles.registerHeaderBlock}>
+                        Вход/Регистрация
+                    </div>
+                </a>
             </header>
         )
 
