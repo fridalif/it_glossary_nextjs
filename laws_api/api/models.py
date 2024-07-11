eae64d0b235a27a0a5669bd913f59a98f364dc5b
fridalif <124243435+fridalif@users.noreply.github.com>
2024-07-11 15:29:26 +0300

    created models in api

--- laws_api/api/models.py
+++ laws_api/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Document(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название документа')
+    file = models.FileField(upload_to='documents/', verbose_name='Файл документа')
+
+class Terms(models.Model):
+    term = models.CharField(max_length=255, verbose_name='Термин')
+    definition = models.CharField(max_length=255, verbose_name='Определение')
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
