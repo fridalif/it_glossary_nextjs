b8065d972b1af2f46f20ba4466dc4968398adf65
fridalif <124243435+fridalif@users.noreply.github.com>
2024-07-13 20:36:31 +0300

    all works correctly

--- components/Body/Body.jsx
+++ components/Body/Body.jsx
@@ -9,10 +9,10 @@ export function Body({mode}) {
     const [api_data, setApiData] = useState([]);
     const [query, setQuery] = useState('');
     const getInfoWithQuery = async ()=>{
-        if(mode!=='Terms' && mode!=='Documents'){
+        if(mode!=='Terms' && mode!=='Documents' && mode!=='Home'){
             return [];
         }
-        if(mode === 'Documents'){
+        if(mode === 'Documents' || mode === 'Home'){
             let response = await axios.get(`http://127.0.0.1:8000/api/documents/?query=${query}`)                
             return response.data
         }
@@ -41,10 +41,10 @@ export function Body({mode}) {
                 }
             </div>
             {
-                mode === 'Register' || mode === 'Login' && <RegisterForm mode={mode} />
+                (mode === 'Register' || mode === 'Login') && <RegisterForm mode={mode} />
             }
             {
-                mode === 'Home' || mode === 'Documents' && api_data.map((item) => <DocumentBubble key={item.id} documentName={item.name} documentId={item.id} downloadsCount={item.downloads} />)
+                (mode === 'Home' || mode === 'Documents') && api_data.map((item) => <DocumentBubble key={item.id} documentName={item.name} documentId={item.id} downloadsCount={item.downloads} />)
             }
             {
                 mode === 'Terms' && api_data.map((item) => <TermBubble key={item.id} term={item.term} definition={item.definition} documentName={item.document_name} documentId={item.document.id} termId={item.id} />)
