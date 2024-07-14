15ef83126364a0c887916cc22fe73d7eb1ecf60d
fridalif <124243435+fridalif@users.noreply.github.com>
2024-07-14 10:58:15 +0300

    added jwt auth in urls.py

--- laws_api/laws_api/settings.py
+++ laws_api/laws_api/settings.py
@@ -129,3 +129,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
